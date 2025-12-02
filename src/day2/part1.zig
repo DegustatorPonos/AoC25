2de78a79d9045f33acc3a34c2da055fb9bfa9903
Physcik <mynameisgennadiy@vk.com>
2025-12-02 17:06:24 +0500

    day 2 part 2 solved

--- src/day2/part1.zig
+++ src/day2/part1.zig
@@ -23,7 +23,6 @@ pub fn ParseInputs(file: []const u8) ![]Span {
         // Edge case in the last element in sequence
         if (endUnparsed[endUnparsed.len - 1] == '\n') 
             endUnparsed = endUnparsed[0..endUnparsed.len - 1];
-        std.debug.print("Start: '{s}', End: '{s}'\n", .{startUnparsed, endUnparsed});
         const start = try std.fmt.parseInt(u64, startUnparsed, 10);
         const end = try std.fmt.parseInt(u64, endUnparsed, 10);
         try outp.append(alloc, Span {
@@ -56,8 +55,45 @@ pub fn SolvePart1(inputs: []const Span) u64 {
     return outp;
 }
 
+pub fn SolvePart2(inputs: []const Span) !u64 {
+    var outp: u64 = 0;
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const alloc = gpa.allocator();
+
+    for (inputs) |inp| {
+        // const len = getSymbolsAmount(inp.Start);
+        var added = std.AutoHashMap(u64, bool).init(alloc);
+        var startIterator: u64 = 1;
+        while (combineNumTwice(startIterator) <= inp.End) {
+            var iteration = combineNumTwice(startIterator);
+            var repetitions: u64 = 2;
+            while (iteration <= inp.End) {
+                if (iteration >= inp.Start and !added.contains(iteration)) {
+                    outp += iteration;
+                    try added.put(iteration, true);
+                }
+                repetitions += 1;
+                iteration = combineNumNTimes(startIterator, repetitions);
+                // std.debug.print("Testing {d}<{d}\n", .{iteration, inp.End});
+            }
+            startIterator += 1;
+        }
+        added.deinit();
+    }
+
+    return outp;
+}
+
 fn combineNumTwice(num: u64) u64 {
     return num + num * math.pow(u64, 10, getSymbolsAmount(num));
+}
+
+fn combineNumNTimes(num: u64, repetitions: u64) u64 {
+    var outp: u64 = num;
+    for (1..repetitions) |i| {
+        outp += num * math.pow(u64, 10, getSymbolsAmount(num) * i);
+    }
+    return outp;
 }
 
 fn divCelling(num: u64, denom: u64) u64 {
@@ -76,6 +112,7 @@ fn getSymbolsAmount(num: u64) u64 {
 
 test "comb" {
     try std.testing.expect(combineNumTwice(12345) == 1234512345);
+    try std.testing.expect(combineNumNTimes(123, 4) == 123123123123);
 }
 
 test "Part 1 solve" {
@@ -91,6 +128,26 @@ test "Part 1 solve" {
         .{.Start = 38593856, .End = 38593862 },
     };
     const result = SolvePart1(&inputs);
-    std.debug.print("Result: {d}, expected: {d} (diff= {d})\n", .{result, expected, @abs(expected - result)});
+    // std.debug.print("Result: {d}, expected: {d} (diff= {d})\n", .{result, expected, @abs(expected - result)});
+    try std.testing.expect(result == expected);
+}
+
+test "Part 2 solve" {
+    const expected: u64 = 4174379265;
+    const inputs = [_]Span {
+        .{.Start = 11, .End = 22},
+        .{.Start = 95, .End = 115},
+        .{.Start = 998, .End = 1012},
+        .{.Start = 1188511880, .End = 1188511890},
+        .{.Start = 1698522, .End = 1698528},
+        .{.Start = 222220, .End = 222224 },
+        .{.Start = 446443, .End = 446449},
+        .{.Start = 38593856, .End = 38593862},
+        .{.Start = 565653, .End = 565659},
+        .{.Start = 824824821, .End = 824824827},
+        .{.Start = 2121212118, .End = 2121212124},
+    };
+    const result = try SolvePart2(&inputs);
+    std.debug.print("Result: {d}, expected: {d} (diff= {d})\n", .{result, expected, @abs(result - expected)});
     try std.testing.expect(result == expected);
 }
--- src/main.zig
+++ src/main.zig
@@ -4,7 +4,7 @@ const d1 = @import("day1/p1.zig");
 const d2 = @import("day2/part1.zig");
 
 pub fn main() !void {
-    try solveDay1();
+    // try solveDay1();
     try solveDay2();
 }
 
@@ -19,7 +19,9 @@ fn solveDay1() !void {
 fn solveDay2() !void {
     const inp = try d2.ParseInputs("input2.txt");
     const p1res = d2.SolvePart1(inp);
+    const p2res = try d2.SolvePart2(inp);
     std.debug.print("Day 2 part 1 result: {d}\n", .{p1res});
+    std.debug.print("Day 2 part 2 result: {d}\n", .{p2res});
 }
 
 test {
